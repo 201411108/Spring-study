8e004924adabce7ce65f770202fc13a753781639
201411108 <aggsae@gmail.com>
2023-04-04 11:06:09 +0900

    feat: thymeleaf 예제 2

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,7 +1,10 @@
 package com.shop.controller;
+
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.time.LocalDateTime;
 @Controller
 @RequestMapping(value = "/thymeleaf")
 public class ThymeleafExController {
@@ -9,5 +12,17 @@ public class ThymeleafExController {
     public String thymeleafExample01(Model model) {
         model.addAttribute("data", "타임리프 예제 입니다.");
         return "thymeleafEx/thymeleafEx01"; // resources/templates 기준 위치
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemName("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
     }
 }
